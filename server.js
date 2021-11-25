bd09756dc79db2f89740613de2a02a0f592fcc02
ykmeracle <ykmeracle@icloud.com>
2021-11-24 23:50:49 -0800

    test commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // building a server with code
 let http = require("http")
 
-let ourApp = http.createServer(function(req, res){
+let ourApp = http.createServer(function(req, res){ //this function sends a REQuest and then listens for  RESponse
     console.log(req.url)
     res.end("Hello, and welcome to our website.") 
 })
 ourApp.listen(3000)
 
 //local host is th name of our computer, 3000 is the listening number
-//type localhost:3000 to open thi sserver in a web browser
+//type localhost:3000 to open this sserver in a web browser
